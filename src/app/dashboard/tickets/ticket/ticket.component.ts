d8cd00b803154d0183bf1681b844254e9c4b698c
Yevhen Dovhan <y.d.devbox@gmail.com>
2024-10-24 11:01:54 +0300

    feat: updating tickets status

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, input, output, signal} from '@angular/core';
+import {Ticket} from "../ticket.model";
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
+  isVisible = signal(true);
 
+  close = output()
+
+  toggle() {
+    this.isVisible.update(v => !v);
+  }
+
+  onCloseTicket() {
+    this.close.emit()
+  }
 }
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -25,4 +25,16 @@ export class TicketsComponent {
     }
     this.tickets.push(newTicket)
   }
+
+  onTicketClosed(ticketId: string) {
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === ticketId) {
+        return {
+          ...ticket,
+          status: 'closed'
+        }
+      }
+      return ticket
+    })
+  }
 }
